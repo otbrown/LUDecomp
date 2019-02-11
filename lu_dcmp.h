a56205ae94339de3f5263516e47bc627ac36c192
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2019-02-11 17:51:13 +0000

    lu_dcmp.h: removed everything but decompLU

--- lu_dcmp.h
+++ lu_dcmp.h
@@ -1,11 +1,6 @@
 #ifndef LU_DCMP_H
 #define LU_DCMP_H
 
-inline int lindex(const int, const int, const int, const int);
-
-double * decompLU(const int, const int, double const * const);
-double * initLUResult(const int, const int);
-void croutBetaSolve(const int, const int, const int, const int, double const *);
-void croutAlphaSolve(const int, const int, const int, const int, double const *);
+void decompLU(const int, double const * const, double * const);
 
 #endif
